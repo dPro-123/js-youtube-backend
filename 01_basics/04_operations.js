ae10c4dfa783ed82e5f62ed63f989f05507eead4
dPro-123 <ghoshdeepratim752006@gmail.com>
2026-06-26 07:10:01 +0000

    operation

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,30 @@
+
+let value=3
+let negValue = -value
+console.log(negvalue) // -3
+
+console.log(2+2);//4
+console.log(2-2) //0
+console.log(2*2) //4
+console.log(2/2)//1
+console.log(2**3)//6
+console.log(2%3)//1
+
+let str1 = "hello"
+let str2="hitesh"
+let str3= str1 + str2;
+console.log(str3);//"hello hitesh"
+console.log(1 + "2")//12
+console.log("1"+"2")//12
+console.log("1"+2+2)//122
+console.log(1+2+"2")//32
+console.log((3+4) * 5 % 3)
+
+console.log(true)//true
+console.log(+true)//1
+console.log(true+)//false syntax
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter)//101
+
