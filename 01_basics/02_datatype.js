b078505c04ce0144ac7be652254e06d3ad9a92b7
dPro-123 <ghoshdeepratim752006@gmail.com>
2026-06-26 05:07:01 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,21 @@
+
+"use strict"; //treat all js code as newer version //
+//alert(3+3)// not working because we are using nodejs, not browser
+
+/* to know about check out tcs39 and mdn */
+
+let name = "hitesh" // string //
+let age = 18 // number //
+let isloggedIn = false // boolean 
+
+//number=> 2 to power 53 //
+//bigint //
+//string =""//
+//boolean => true /false //
+//null =>standalone value //
+//undefined => not defined //
+//symbol => unique //
+
+//object//
+
+console.log(typeof"hitesh")//give type
