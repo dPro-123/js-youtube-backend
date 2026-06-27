34667f0c82469ff9be1039d62ef4ee49874fd6eb
dPro-123 <ghoshdeepratim752006@gmail.com>
2026-06-27 12:15:58 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,8 @@
+
+const name="hitesh"
+const repoCount=50
+//console.log(name+repocount+"value")//old technique
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);//new technique(string interpolation)
+
+const gameName=new String('hitesh')//string declaration using object declaration// 
