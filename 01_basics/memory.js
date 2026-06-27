4df1cb39fd4b8beaa43978295c8f8760c3994a2b
dPro-123 <ghoshdeepratim752006@gmail.com>
2026-06-27 05:58:11 +0000

    memory(stack and heap)

---
+++ 01_basics/memory.js
@@ -0,0 +1,18 @@
+
+//stack(primitive),heap(non-primitive)
+let myYoutubename="hiteshchowdhurydotcom"
+let anothername=myYoutubename
+anothername="chaiaurcode"
+console.log(myYoutubename)
+console.log(anothername)
+console.log(anothername)
+
+let userOne={
+    email:"user@gmail.com",
+    upi:"user@ybl"
+
+}
+let userTwo=userOne
+userTwo.email="ghosh@gmail.com"
+console.log(userOne.email)
+console.log(userTwo.email)
